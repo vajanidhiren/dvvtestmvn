022a1881932c67936fcb19b14c754dd02bb15610
dhiren.vajani <dhiren.vajani@stl.tech>
2021-03-30 21:37:40 +0530

    Changing compiler plugin to java source 1.8

--- src/main/java/dvv/test/pkg1/Application.java
+++ src/main/java/dvv/test/pkg1/Application.java
@@ -1,7 +1,20 @@
 package dvv.test.pkg1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application
 {
+	public void greet()
+	{
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+
+		for(String greeting : greetings)
+		{
+			System.out.println("Greeting: "+greeting);
+		}
+	}
 	public Application()
 	{
 		System.out.println("Application constructor called");
@@ -11,5 +24,6 @@ public class Application
 	{
 		System.out.println("main() method of Application called");
 		Application app = new Application();
+		app.greet();
 	}
 }
