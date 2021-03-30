ab18dc6c946178b6298f582b39cd28a96236bf2e
dhiren.vajani <dhiren.vajani@stl.tech>
2021-03-30 21:59:45 +0530

    Use of StringUtils and added dependency to lang3

--- src/main/java/dvv/test/pkg1/Application.java
+++ src/main/java/dvv/test/pkg1/Application.java
@@ -2,9 +2,15 @@ package dvv.test.pkg1;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application
 {
+	public int countWords(String words)
+	{
+		String[] separatewords = StringUtils.split(words,' ');
+		return(separatewords == null) ? 0 : separatewords.length;
+	}
 	public void greet()
 	{
 		List<String> greetings = new ArrayList<>();
@@ -25,5 +31,7 @@ public class Application
 		System.out.println("main() method of Application called");
 		Application app = new Application();
 		app.greet();
+		int count = app.countWords("Be positive and think positive");
+		System.out.println("No of words : "+count);
 	}
 }
